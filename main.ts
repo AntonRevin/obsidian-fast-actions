572ff23bb4836b99f6575ccd61f5bac220504017
Anton <anton@revin.io>
2024-05-26 21:46:41 +0200

    V0.1 functionality implemented for Star/Action/Question toggle commands.

--- main.ts
+++ main.ts
@@ -1,86 +1,56 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface FastActionsSettings {
+	star: string,
+	action: string,
+	question: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: FastActionsSettings = {
+	star: '#key',
+	action: '#action',
+	question: "#question"
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class FastActions extends Plugin {
+	settings: FastActionsSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// Star
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'star-toggle',
+			name: 'Toggle Star',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
+				this.lineToggleValue(editor, this.settings.star);
+			},
+			icon: "star"
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
+
+		// Action
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+			id: 'action-toggle',
+			name: 'Toggle Action',
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				this.lineToggleValue(editor, this.settings.action);
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		// Question
+		this.addCommand({
+			id: 'question-toggle',
+			name: 'Toggle Question',
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				this.lineToggleValue(editor, this.settings.question);
+			}
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// Register the settings tab
+		this.addSettingTab(new SettingsTab(this.app, this));
 	}
 
-	onunload() {
-
-	}
+	onunload() { }
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -89,46 +59,97 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	lineToggleValue(editor: Editor, val: string): void {
+		var ln = editor.getCursor();
+		var txt = editor.getLine(ln.line);
+
+		var toggleType = txt.includes(val + " ");
+		var checkbox = RegExp(/([-+*].(\[.\]))/).test(txt);
+
+		if (toggleType) {
+			txt = txt.replace(new RegExp(val + " ", "g"), "");
+		} else {
+			if (txt.length > 0) {
+				var txtTrim = txt.trimStart();
+				var startPos = txt.search(/\S|$/);
+				if (txtTrim[0] == "-" || txtTrim[0] == "+") {
+					if (checkbox) {
+						txt = txt.slice(0, startPos) + "- [" + txt[startPos + 3] + "] " + val + txt.slice(startPos + 5);
+					} else {
+						txt = txt.slice(0, startPos) + "- " + val + txt.slice(startPos + 1);
+					}
+				} else {
+					if (startPos == 0) {
+						txt = val + " " + txt.slice(startPos);
+					} else {
+						txt = txt.slice(0, startPos) + val + " " + txt.slice(startPos + 1);
+					}
+				}
+			}
+		}
+		editor.setLine(ln.line, txt);
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class SettingsTab extends PluginSettingTab {
+	plugin: FastActions;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: FastActions) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
+		containerEl.createEl("h1", {
+			text: "Fast Actions - Settings",
+		});
+
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Star')
+			.setDesc('String toggle using the Star command')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('#key')
+				.setValue(this.plugin.settings.star)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.star = value;
 					await this.plugin.saveSettings();
 				}));
+
+		new Setting(containerEl)
+			.setName('Action')
+			.setDesc('String toggle using the Action command')
+			.addText(text => text
+				.setPlaceholder('#action')
+				.setValue(this.plugin.settings.action)
+				.onChange(async (value) => {
+					this.plugin.settings.action = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Question')
+			.setDesc('String toggle using the Question command')
+			.addText(text => text
+				.setPlaceholder('#question')
+				.setValue(this.plugin.settings.question)
+				.onChange(async (value) => {
+					this.plugin.settings.question = value;
+					await this.plugin.saveSettings();
+				}));
+
+		containerEl.createEl("p", {
+			text: "Fast Actions - Settings",
+		});
+
+		containerEl.createEl("a", {
+			text: "//AR",
+			cls: "fastactions_logo",
+			href: "https://www.linkedin.com/in/antonrevin/"
+		});
 	}
 }
